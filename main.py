9861711ad48ace391a26b423a97233e994192f82
gjordanarincon <jordanarincon.guillem@lfbogota.com>
2024-05-29 13:09:39 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
-print("hola")
+car = "+-/*!&$#?=@abcdefghijklnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
+import random
+con=int(input("ingrese la longitud de la contraseña"))
+password = ""
+for i in range(con):
+    password+=random.choice(car)
+print(password)
+    
